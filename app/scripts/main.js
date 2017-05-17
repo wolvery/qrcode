c2bb8997cf1d576b589cc9442d1cd9f17cb4e949
Guilherme Goncalves <guilherme.silva@remopt.com>
2017-05-17 20:53:26 -0300

    Use of web worker and requestanimation frame to canvas.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,7 +16,7 @@
  *  limitations under the License
  *
  */
-(function() {
+ (function() {
   'use strict';
 
   var QRCodeCamera = function(element) {
@@ -28,14 +28,14 @@
 
     cameraManager.onframe = function() {
       // There is a frame in the camera, what should we do with it?
- 
+
       var imageData = cameraManager.getImageData();
       var detectedQRCode = qrCodeManager.detectQRCode(imageData, function(url) {
         if(url !== undefined) {
           qrCodeManager.showDialog(url);
         }
       });
-    
+
     };
   };
 
@@ -45,7 +45,7 @@
     var qrcodeData = root.querySelector(".QRCodeSuccessDialog-data");
     var qrcodeNavigate = root.querySelector(".QRCodeSuccessDialog-navigate");
     var qrcodeIgnore = root.querySelector(".QRCodeSuccessDialog-ignore");
-
+    var qrWorker = new Worker("scripts/jsqrcode/qrworker.js");
     var client = new QRClient();
 
     var self = this;
@@ -56,38 +56,42 @@
     this.detectQRCode = function(imageData, callback) {
       callback = callback || function() {};
 
-      client.decode(imageData, function(result) {
-        if(result !== undefined) {
-          self.currentUrl = result;
-        }
-        callback(result);
-      });
+      qrWorker.postMessage(imageData);
+
+      qrWorker.onmessage = function(e){
+       if(e.data !== undefined) {
+        self.currentUrl = e.data;
+      }
+      callback(e.data);
     };
 
-    this.showDialog = function(url) {
-      root.style.display = 'block';
-      qrcodeData.innerText = url;
-    };
+  };
 
-    this.closeDialog = function() {
-      root.style.display = 'none';
-      self.qrcodeNavigate = "";
-      qrcodeData.innerText = "";
-    };
 
-    qrcodeIgnore.addEventListener("click", function() {
-      self.closeDialog();
-    }.bind(this));
+  this.showDialog = function(url) {
+    root.style.display = 'block';
+    qrcodeData.innerText = url;
+  };
 
-    qrcodeNavigate.addEventListener("click", function() {
+  this.closeDialog = function() {
+    root.style.display = 'none';
+    self.qrcodeNavigate = "";
+    qrcodeData.innerText = "";
+  };
+
+  qrcodeIgnore.addEventListener("click", function() {
+    self.closeDialog();
+  }.bind(this));
+
+  qrcodeNavigate.addEventListener("click", function() {
       // I really want this to be a link.
       window.location = this.currentUrl;
       this.closeDialog();
     }.bind(this));
 
-  };
+};
 
-  var CameraManager = function(element) {
+var CameraManager = function(element) {
     // The camera gets a video stream, and adds it to a canvas.
     // The canvas is analysed but also displayed to the user.
     // The video is never show
@@ -105,7 +109,7 @@
 
     // Variables
     var dWidth;
-    var dHeight; 
+    var dHeight;
     var dx = 0;
     var dy = 0;
 
@@ -150,19 +154,19 @@
         overlayCoords.height = cameraCanvas.height - (boxHeightSize * 2);
 
       }
-     
+
     };
 
     var setupVariables = function(e) {
       dWidth = cameraCanvas.width = window.innerWidth;
-      dHeight = cameraCanvas.height = window.innerHeight; 
+      dHeight = cameraCanvas.height = window.innerHeight;
       dx = 0;
       dy = 0;
 
       sx = 0;
       sy = 0;
 
-      // Make the video coordinate space the same as the window. 
+      // Make the video coordinate space the same as the window.
       // size in the longest dimension.
       // Then center and clip. and map back to correct space.
       scaleX = (dWidth / cameraVideo.videoWidth);
@@ -172,7 +176,7 @@
       // Trim the left
       sx = ((cameraVideo.videoWidth * scaleFactor) / 2) - (dWidth/ 2);
       sy = ((cameraVideo.videoHeight * scaleFactor) / 2) - (dHeight / 2);
-     
+
       // Trim the right.
       sWidth = (cameraVideo.videoWidth * scaleFactor) - sx * 2;
       sHeight = (cameraVideo.videoHeight * scaleFactor) - sy * 2;
@@ -191,6 +195,8 @@
       if(self.onframe) self.onframe();
 
       coordinatesHaveChanged = false;
+
+      requestAnimationFrame(captureFrame);
     };
 
     var getCamera = function(videoSource, cb) {
@@ -198,30 +204,30 @@
       cb = cb || function() {};
 
       var gUM = (navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia || null);
+       navigator.webkitGetUserMedia ||
+       navigator.mozGetUserMedia ||
+       navigator.msGetUserMedia || null);
 
       var params;
 
       if(videoSource === undefined && cameras.length == 0) {
         // Because we have no source information, have to assume it user facing.
-        params = { video: true }; 
+        params = { video: true };
       }
       else {
         params = { video: { optional: [{sourceId: videoSource.id}] } };
       }
-  
+
       gUM.call(navigator, params, function(theStream) {
         localStream = theStream;
-        
+
         cameraVideo.onloadeddata = function(e) {
 
           coordinatesHaveChanged = true;
-          
+
           var isSetup = setupVariables(e);
           if(isSetup) {
-            setInterval(captureFrame.bind(self), 4);
+            requestAnimationFrame(captureFrame.bind(self));
           }
           else {
             // This is just to get around the fact that the videoWidth is not
@@ -229,14 +235,14 @@
             setTimeout(function() {
               setupVariables(e);
 
-              setInterval(captureFrame.bind(self), 4);
+              requestAnimationFrame(captureFrame.bind(self));
             }, 100);
           }
 
           // The video is ready, and the camerea captured
           if(videoSource === undefined) {
             // There is no meta data about the camera, assume user facing.
-            videoSource = { 
+            videoSource = {
               'facing': 'user'
             };
           }
@@ -327,7 +333,7 @@
     });
 
     // Init
-    getSources(function() { 
+    getSources(function() {
       // On first run, select the first camera.
       getCamera(cameras[0], toggleFacingState);
     });
